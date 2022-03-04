3b9247e889d5cc77eb0a9798a223a58ac444d695
Jiawei Ma <jiaweima@cmail.carleton.ca>
2022-03-04 04:26:11 -0500

    Make change on HttpRequestTest

--- src/test/java/simpleSpringMVC/HttpRequestTest.java
+++ src/test/java/simpleSpringMVC/HttpRequestTest.java
@@ -1,26 +1,37 @@
 package simpleSpringMVC;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class HttpRequestTest {
 
-    @LocalServerPort
-    private int port = 8080;
-
     @Autowired
-    private TestRestTemplate restTemplate = new TestRestTemplate();
+    MockMvc mockMvc;
 
     @Test
-    public void mainPageReturnIndex() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class)).contains("index");
+    public void getAddressBook() throws Exception {
+        this.mockMvc.perform(get("/addressBook/1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"id\"")));
+        this.mockMvc.perform(get("/addressBook?id=1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"id\"")));
     }
 }
